a29243c9e0aaef4d7cb493842834029a93420e0e
hungnm7 <hungnm7@pvcombank.com.vn>
2025-03-04 10:26:32 +0700

    config baas

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -237,7 +237,7 @@ export default env => {
         },
       }),
       new Repack.plugins.ModuleFederationPlugin({
-        name: 'MiniApp1',
+        name: 'baas',
         exposes: {
           './MiniAppNavigator': './src/navigation/MainNavigator',
         },
