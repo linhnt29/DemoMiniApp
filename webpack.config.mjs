07c414ba7aa2ae4fb4c023731d8f83a2f6ad2056
hungnm7 <hungnm7@pvcombank.com.vn>
2025-03-04 10:26:58 +0700

    config ba

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -237,7 +237,7 @@ export default env => {
         },
       }),
       new Repack.plugins.ModuleFederationPlugin({
-        name: 'MiniApp1',
+        name: 'ba',
         exposes: {
           './MiniAppNavigator': './src/navigation/MainNavigator',
         },
