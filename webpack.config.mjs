4fbacd4d4b17d8fedae30bfea15dc2454aa1af9f
hungnm7 <hungnm7@pvcombank.com.vn>
2025-03-04 10:26:22 +0700

    config retail

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -237,7 +237,7 @@ export default env => {
         },
       }),
       new Repack.plugins.ModuleFederationPlugin({
-        name: 'MiniApp1',
+        name: 'retail',
         exposes: {
           './MiniAppNavigator': './src/navigation/MainNavigator',
         },
